c4ab24ce416e67977d60f06fa6aad121b6390706
zengjie <thezj@vip.qq.com>
2017-10-09 10:42:02 +0800

    test vscode client push

--- 109.py
+++ 109.py
@@ -11,6 +11,7 @@ tuple3 = tuple1 + tuple2
 print(tuple3)
 del tuple3
 # print(tuple3) name 'tuple3' is not defined
+
 #  max(tuple1) not supported between instances of 'str' and 'int'
 print(len(tuple2), tuple1 + (4, ), tuple2 * 1, 3 in tuple1, tuple([2, 3]))
 for x in tuple1:
